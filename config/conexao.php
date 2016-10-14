1e8136772e6959923c3ace4e7f15ca07ebb839c3
Faria Quitamba <fariaquitamba5@gmail.com>
2016-10-14 11:59:23 +0100

    fx

--- config/conexao.php
+++ config/conexao.php
@@ -7,7 +7,7 @@ class conexao {
 		$this->banco = NewADOConnection ("mysql");
 		$this->banco->dialect=3;
 		$this->banco->debug=false;
-		$this->banco->connect ("localhost", "root", "", "doar")  or die($this->banco->ErrorMsg());
+		$this->banco->connect ("localhost", "root", "u162066525_root", "u162066525_doar")  or die($this->banco->ErrorMsg());
 		$this->banco->Execute("SET NAMES 'utf8'");
 		$this->banco->Execute('SET character_set_connection=utf8');
 		$this->banco->Execute('SET character_set_client=utf8');
